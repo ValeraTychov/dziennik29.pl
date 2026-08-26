d4f53e6a07ad3ed1a7929ef5c88af0775bc45cc8
ValeraTychov <yury.rasokha@gmail.com>
2026-08-26 21:59:09 +0200

    add interactive page references from D29.1 and D29.2. Allign answers.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import GamePage from './pages/GamePage';
 import NotFound from './pages/NotFound';
 import Notes from './pages/Notes';
 import Mirror from './components/Mirror';
-import Video from './components/Video';
 
 function App() {
 	return (
@@ -14,23 +13,23 @@ function App() {
 				<Route path="/:pageId" element={<GamePage />} />
 
 				{/* Static */}
-				<Route path="/notes"   element={<Notes />} />
-				<Route path="/notatki" element={<Notes />} />
-
-				{/* YouTube */}
-				<Route path="/rozpadlina" element={<Video videoId="qmbigyIDKYI" />} />
+				<Route path="/notes"    element={<Notes />} />
+				<Route path="/notatki"  element={<Notes />} />
 
 				{/* Mirrors */}
-				<Route path="/kamienie" element={<Mirror path="stones"    />} />
-				<Route path="/klan"     element={<Mirror path="cloud"     />} />
-				<Route path="/cisza"    element={<Mirror path="silence"   />} />
-				<Route path="/klasyk"   element={<Mirror path="cave"      />} />
-				<Route path="/machina"  element={<Mirror path="apparatus" />} />
-				<Route path="/zapalone" element={<Mirror path="lightson"  />} />
-				<Route path="/zgaszone" element={<Mirror path="lightsoff" />} />
-				<Route path="/dial"     element={<Mirror path="dial"      />} />
-				<Route path="/patrz"    element={<Mirror path="watch"     />} />
-				<Route path="/granit"   element={<Mirror path="undergo"   />} />
+				<Route path="/rozpadlina"  element={<Mirror path="wormroot"   />} />
+				<Route path="/niezbadane"  element={<Mirror path="checkpoint" />} />
+				<Route path="/portal"      element={<Mirror path="entrance"   />} />
+				<Route path="/kamienie"    element={<Mirror path="stones"     />} />
+				<Route path="/klan"        element={<Mirror path="cloud"      />} />
+				<Route path="/cisza"       element={<Mirror path="silence"    />} />
+				<Route path="/klasyk"      element={<Mirror path="cave"       />} />
+				<Route path="/machina"     element={<Mirror path="apparatus"  />} />
+				<Route path="/zapalone"    element={<Mirror path="lightson"   />} />
+				<Route path="/zgaszone"    element={<Mirror path="lightsoff"  />} />
+				<Route path="/dial"        element={<Mirror path="dial"       />} />
+				<Route path="/patrz"       element={<Mirror path="watch"      />} />
+				<Route path="/granit"      element={<Mirror path="undergo"    />} />
 
 				<Route path="*" element={<NotFound />} />
 			</Routes>
